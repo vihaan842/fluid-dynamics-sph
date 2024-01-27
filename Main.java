97a914f4e7d256e8e52f0ea2cb8dc355078145c1
radicise <93229379+radicise@users.noreply.github.com>
2024-01-27 16:27:28 -0500

    Additional features that are not enabled

--- Main.java
+++ Main.java
@@ -31,6 +31,7 @@ class Display extends Canvas {
 	Simulation sim;
 	int radius;
 	double pressureCoeff = 150.0;
+	double mvmtCoeff = 150.0;
 	Display(int x, int y, Simulation s) {
 		viewWidth = x;
 		viewHeight = y;
@@ -42,40 +43,69 @@ class Display extends Canvas {
 		//TODO freeze display when re-painting
 		//g.clearRect(0, 0, viewWidth, viewHeight);
 		double maxPressure = 0.0;
+		double maxMvmt = 0.0;
 		int pressureSamplingRate = 10;
 		
 		for (int j = 0; j < viewHeight; j += pressureSamplingRate) {
 			for (int i = 0; i < viewWidth; i += pressureSamplingRate) {
-				double pres;
-				int pressure = (int) ((pres = Math.pow(pressure(((double) i) / scaling, ((double) j) / scaling), 0.2)) * pressureCoeff);
+				double maxColor;
+				double[] prs = pressure(((double) i) / scaling, ((double) j) / scaling);
+				double pressure = Math.pow(prs[0], 0.2);
+				double dx = 127.9 + (prs[1] * mvmtCoeff);
+				double dy = 127.9 + (prs[2] * mvmtCoeff);
+				double pres = pressure;
+				pressure *= pressureCoeff;
+				if (pressure == 0.0) {
+					dx = 0;
+					dy = 0;
+				}
+				double mC = Math.max(Math.abs(prs[1]), Math.abs(prs[2])) * 2.0;
 				if (pressure > 255) {
 					pressure = 255;
+				}
+				if (dx >= 256) {
+					dx = 255;
+				}
+				else if (dx < 0) {
+					dx = 0;
+				}
+				if (dy > 255) {
+					dy = 255;
+				}
+				else if (dy < 0) {
+					dy = 0;
 				}
 				g.setColor(new Color((int) pressure, (int) pressure, (int) pressure));
 				g.fillRect(i, j, pressureSamplingRate, pressureSamplingRate);
 				if (pres > maxPressure) {
 					maxPressure = pres;
 				}
+				if (mC > maxMvmt) {
+					maxMvmt = mC;
+				}
 			}
 		}
 		pressureCoeff = 200.0 / maxPressure;
+		mvmtCoeff = 200.0 / maxMvmt;
 		g.setColor(new Color(255, 255, 255));
-		g.drawString("Fluid Simulator", 40, 940);
-		g.drawString("t=" + Double.toString(sim.time) + "s", 40, 980);
+		g.drawString("Fluid Simulator", 40, 990);
+		g.drawString("t=" + Double.toString(sim.time) + "s", 40, 1010);
 		
-
 		/*
+		g.clearRect(0, 0, viewWidth, viewHeight);
 		for (double[] pos : sim.positions) {
 			g.drawOval((int) (pos[0] * scaling), (int) (pos[1] * scaling), radius, radius);
 		}
 		*/
 	}
-	double pressure(double x, double y) {
+	double[] pressure(double x, double y) {
 		double dim1min = x - Simulation.diameter;
 		double dim1max = x + Simulation.diameter;
 		double dim2min = y - Simulation.diameter;
 		double dim2max = y + Simulation.diameter;
 		double s = 0.0;
+		double dx = 0.00;
+		double dy = 0.00;
 		double[] r = new double[]{x, y};
 		for (int j = 0; j < sim.PARTICLES; j++) {
 		    if (sim.positions[j][0] <= dim1min) {
@@ -91,7 +121,15 @@ class Display extends Canvas {
 			continue;
 		    }
 		    s += sim.kernel(Simulation.subtract(r, sim.positions[j]));
+		    dx += s * sim.velocities[j][0];
+		    dy += s * sim.velocities[j][1];
 		}
-		return Simulation.PRESSURE_CONSTANT * Math.pow(s, Simulation.PRESSURE_POWER);
+		if (dx != 0.0) {
+			dx /= s;
+		}
+		if (dy != 0.0) {
+			dy /= s;
+		}
+		return new double[]{Simulation.PRESSURE_CONSTANT * Math.pow(s, Simulation.PRESSURE_POWER), dx, dy};
 	}
 }
